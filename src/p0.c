55ab9edae8f3fb73e82e86d6b4b526253fcc9640
Branden Kim <brandenk0@gmail.com>
2017-10-01 17:58:55 -0400

    Initial files created

---
+++ src/p0.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int main() {
+    // Create the named pipes
+    char *p1 = "/tmp/p1";
+    char *p2 = "/tmp/p2";
+    char *p3 = "/tmp/p3";
+
+    
+}
